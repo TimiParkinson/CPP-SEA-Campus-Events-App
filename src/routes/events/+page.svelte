cb467eb12733105fd9986c572286f7f781ae0138
Lily in the Wind <spookyboe2@gmail.com>
2025-09-29 10:15:49 -0700

    Added event type tabs and event tabs (no redirects/input yet)

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,2 +1,71 @@
-<p>HI!!!!!</p>
+<main>
+    <h1>Your Events</h1>
 
+    <div id="selectionTabs" style="display:flex">
+        <div id="tab">
+            <p> E-Sports </p>
+        </div>
+        <div id="tab">
+            <p> SEA Events </p>
+        </div>
+        <div id="tab">
+            <p> Workshops </p>
+        </div>
+        <div id="tab">
+            <p> Bronco Pride </p>
+        </div>
+        <div id="tab">
+            <p> NSBE Events </p>
+        </div>
+    </div>
+
+    <div id="eventTabs" style="display:flex">
+        <div id="event">
+            <!-- <img src="imageSRC.jpg" alt="ScreenReaderText"> -->
+            <div id="fakeImageReplaceLater">
+            </div>
+            <h2>Code & Coffee</h2>
+            09/29/2025
+        </div>
+        <div id="event">
+            <!-- <img src="imageSRC.jpg" alt="ScreenReaderText"> -->
+            <div id="fakeImageReplaceLater">
+            </div>
+            <h2>SEA Tech Talks</h2>
+            09/30/2025
+        </div>
+    </div>
+</main>
+<style>
+    main{
+        margin-left: 4px;
+        height: 480px;
+    }
+    h1{
+        font-size: 2em;
+    }
+    p{
+        margin-left: 4px;
+        margin-right: 4px;
+    }
+    #tab{
+        border-radius: 5px;
+        border-width: 2px;
+        border-color: rgb(193, 193, 193);
+        margin-left: 2px;
+        margin-right: 2px;
+    }
+    #selectionTabs{
+        margin-bottom: 16px;
+        margin-top: 20px;
+    }
+    #fakeImageReplaceLater{
+        background-color: blue;
+        height:160px;
+        width:200px;
+        border-radius: 16px;
+    }
+    #event{
+        margin-right: 8px;
+    }
+</style>
