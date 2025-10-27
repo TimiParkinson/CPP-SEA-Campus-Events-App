78bf0c70f8340e2f1ecbe44090152b6dcd6727dc
lebuckman <liambuckman0@gmail.com>
2025-10-26 20:02:50 -0700

    refactor: extract search bar into reusable component

---
+++ src/lib/components/SearchBar.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import * as InputGroup from '$lib/components/ui/input-group/index.js';
+	import { Search } from '@lucide/svelte';
+
+	interface Props {
+		value: string;
+		onInput: (value: string) => void;
+		placeholder?: string;
+	}
+
+	let { value, onInput, placeholder = 'Search here...' }: Props = $props();
+</script>
+
+<InputGroup.Root>
+	<InputGroup.Input {placeholder} {value} oninput={(e) => onInput(e.currentTarget.value)} />
+	<InputGroup.Addon>
+		<Search />
+	</InputGroup.Addon>
+</InputGroup.Root>
