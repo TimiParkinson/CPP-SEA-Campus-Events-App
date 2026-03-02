a6b559497b82cd413bb5df14db1eb3ed0b362711
Clarence Ballensky <alballensky@cpp.edu>
2026-03-02 15:33:26 -0800

    feat: added headers and an input box

---
+++ src/routes/(pages)/CRUD/+page.svelte
@@ -0,0 +1,17 @@
+<script lang=ts>
+
+    import { Input } from "$lib/components/ui/input/index.js";
+    import TextArea from "$lib/components/ui/textarea/textarea.svelte";
+
+</script>
+
+<div class="pt-24">
+
+    <h1 class="text-4xl mb-8 leading-tight font-bold">Edit Your Organization</h1>
+
+    <h2 class="mb-4 text-xl font-bold sm:text-2xl">Name</h2>
+    <Input type="orgName" placeholder="Organization Name" class="max-w-xs mb-4" />
+
+    <h2 class="mb-4 text-xl font-bold sm:text-2xl">Description</h2>
+    
+</div>
